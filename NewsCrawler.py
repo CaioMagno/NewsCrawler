17f306805c9110c542338043e5d36261397260cc
Caio Magno <magno.caio@yahoo.com.br>
2017-11-30 22:15:01 -0300

    Acrescentei um crawler para a Folha

--- NewsCrawler.py
+++ NewsCrawler.py
@@ -30,6 +30,12 @@ class NewsScrapper:
     def getNewsContent(self, link):
         pass
 
+    def getNewsHeadline(self, link):
+        pass
+
+    def getPublishDate(self, link):
+        pass
+
     def getNewsArticlesParsed(self, keywordsList, numPage):
         pass
 
@@ -95,7 +101,53 @@ class NewsScrapperG1 (NewsScrapper):
 
         return parsedLinks
 
+class NewsScrapperFolha(NewsScrapper):
+    def __init__(self):
+        self.request = "http://search.folha.uol.com.br/?q="
+        self.urlLinksTagClass = "busca-titulo"
+        self.contentTextTagClass = "content-text__container"
+        self.headlineTagClass = "content-head__title"
+
+    def buildSearchRequest(self, keywordsList, numPage):
+        query = '+'.join(keywordsList)
+        request = self.request + query
+        return request
+
+    def getLinksInResponseContent(self, response):
+        soup = BeautifulSoup(response.text, "lxml")
+        h3 = soup.find_all(name="h3", class_="search-results-title")
+        urls = [htmlBlock.find("a")["href"] for htmlBlock in h3]
+        return urls
+
+    def getNewsHeadline(self, pageSoup):
+        headline = pageSoup.find(name="h1", itemprop="headline").text
+        return headline
+
+    def getPublishDate(self, pageSoup):
+        date_published = pageSoup.article.time.text.strip()
+        return date_published
+
+    def getNewsContent(self, pageSoup):
+        article_body = pageSoup.find(name="div", class_="content", itemprop="articleBody")
+        if article_body:
+            paragraphs = article_body.find_all(name="p")
+            paragraphsText = [paragraph.text.strip("\n") for paragraph in paragraphs]
+            return paragraphsText
+        else:
+            return None
+
+    def getNewsArticlesParsed(self, keywordsList, numPage):
+        url = self.buildSearchRequest(keywordsList, numPage)
+        response = requests.get(url)
+        links = self.getLinksInResponseContent(response)
+        pages = [requests.get(link) for link in links]
+        pagesSoup = [BeautifulSoup(page.text, "lxml") for page in pages]
+        Article = namedtuple("Article", "url date headline texts")
+        parsedLinks = {index: Article(url=page[0], date=self.getPublishDate(page[1]), headline=self.getNewsHeadline(page[1]), texts=self.getNewsContent(page[1])) for index, page in enumerate(zip(links, pagesSoup))}
+        return parsedLinks
+
+
 if __name__ == "__main__":
-    scrapper = NewsScrapperG1()
+    scrapper = NewsScrapperFolha()
     results = scrapper.getNewsArticlesParsed(["temer", "pmdb"], 1)
     print(results)
